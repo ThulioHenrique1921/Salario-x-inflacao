7c8ce38ea0d1ea49f49bb00b97642227dc67f3dd
Thulio Henrique <thuliohsm2003@gmail.com>
2026-05-05 21:37:03 -0300

    commit 05-05 03

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const entradaDados =  require(`readline-sync`);
 /* IMPORTAÇÃO BIBLIOTECA READLINE-SYNC */
 
 /* DECLARÇÃO DE VARIÁVEIS GLOBAIS */
-let dados_inflacao = [
+let dadosInflacao = [
     {ano: 2010,             salario:510.00,             ipca:5.91                 },
     {ano: 2011,             salario:545.00,             ipca:6.50                },
     {ano: 2012,             salario:622.00,             ipca:5.84                 },
@@ -16,10 +16,10 @@ let dados_inflacao = [
     {ano: 2019,             salario:998.00,             ipca:4.31                 },
     {ano: 2020,             salario:1045.00,            ipca:3.52                 }
 ]
-let label_ano = "Ano: ";
-let label_salario = "Salário mínimo: ";
-let label_IPCA = "Inflação IPCA: ";
-let label_crescimento_salarial = "Crescimento Salarial: ";
+let labelAno = "Ano: ";
+let labelSalario = "Salário mínimo: ";
+let label_Ipca = "Inflação IPCA: ";
+let labelcrescimentoSalarial = "Crescimento Salarial: ";
 /* DECLARÇÃO DE VARIÁVEIS GLOBAIS */
 
 
@@ -31,37 +31,37 @@ let valor_escolha = entradaDados.question("Digite o número da sua escolha: ");
 
 switch (valor_escolha) {
     case "1" :         
-        for (let Dados of dados_inflacao){
-            let salario_formatado = Dados.salario.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
-            console.log(`\n${label_ano.padEnd(40, '.')} ${Dados.ano}\n${label_salario.padEnd(40, '.')} ${salario_formatado}\n` );
+        for (let dados of dadosInflacao){
+            let salarioFormatado = dados.salario.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
+            console.log(`\n${labelAno.padEnd(40, '.')} ${dados.ano}\n${labelSalario.padEnd(40, '.')} ${salarioFormatado}\n` );
         }        
         break;
     case "2" :
-        for (let Dados of dados_inflacao){            
-            console.log(`\n${label_ano.padEnd(40, '.')} ${Dados.ano}\n${label_IPCA.padEnd(40, '.')}${Dados.ipca}%\n`);
+        for (let dados of dadosInflacao){            
+            console.log(`\n${labelAno.padEnd(40, '.')} ${dados.ano}\n${label_Ipca.padEnd(40, '.')}${dados.ipca}%\n`);
         }     
         break;
     case "3" :
-        let Salario_anterior = null;
-        let diferenca_salarial = null;
-        for (let Dados of dados_inflacao){
-            let salario_formatado = Dados.salario.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
-            if (Dados.ano === 2010){
-                let resultado = `\n${label_ano.padEnd(40, '.')} ${Dados.ano}\n`;
-                resultado += `${label_salario.padEnd(40, '.')} ${salario_formatado}\n`;
-                resultado += `${label_crescimento_salarial.padEnd(40, '.')} -\n`;
-                resultado += `${label_IPCA.padEnd(40, '.')}${Dados.ipca}%\n`;
+        let SalarioAnterior = null;
+        let diferencaSalarial = null;
+        for (let dados of dadosInflacao){
+            let salarioFormatado = dados.salario.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
+            if (dados.ano === 2010){
+                let resultado = `\n${labelAno.padEnd(40, '.')} ${dados.ano}\n`;
+                resultado += `${labelSalario.padEnd(40, '.')} ${salarioFormatado}\n`;
+                resultado += `${labelcrescimentoSalarial.padEnd(40, '.')} -\n`;
+                resultado += `${label_Ipca.padEnd(40, '.')} ${dados.ipca}%\n`;
                 console.log(resultado);
-                Salario_anterior = Dados.salario;
+                SalarioAnterior = dados.salario;
             }else {
-                diferenca_salarial = Dados.salario - Salario_anterior;
-                let percentual_de_crescimento = (diferenca_salarial/Salario_anterior) * 100;
-                let resultado = `\n${label_ano.padEnd(40, '.')} ${Dados.ano}\n`;
-                resultado += `${label_salario.padEnd(40, '.')} ${salario_formatado}\n`;
-                resultado += `${label_crescimento_salarial.padEnd(40, '.')} ${percentual_de_crescimento.toFixed(2)}%\n`;
-                resultado += `${label_IPCA.padEnd(40, '.')}${Dados.ipca}%\n`;
+                diferencaSalarial = dados.salario - SalarioAnterior;
+                let percentualCrescimento = (diferencaSalarial / SalarioAnterior) * 100;
+                let resultado = `\n${labelAno.padEnd(40, '.')} ${dados.ano}\n`;
+                resultado += `${labelSalario.padEnd(40, '.')} ${salarioFormatado}\n`;
+                resultado += `${labelcrescimentoSalarial.padEnd(40, '.')} ${percentualCrescimento.toFixed(2)}%\n`;
+                resultado += `${label_Ipca.padEnd(40, '.')} ${dados.ipca}%\n`;
                 console.log(resultado);
-                Salario_anterior = Dados.salario;
+                Salario_anterior = dados.salario;
             }                
         }
         break;
